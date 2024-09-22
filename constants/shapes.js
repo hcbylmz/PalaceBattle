f68d18d029d2342c1eb9c52d6d08951a2aaeff96
Hicabi YILMAZ <45284106+hcbylmz@users.noreply.github.com>
2024-09-22 23:21:02 +0300

    exported constants to a file

---
+++ constants/shapes.js
@@ -0,0 +1,66 @@
+
+export const colors = ["#800000", "#9A6324", "#f032e6", "#3cb44b", "#f58231", "#ffd8b1", "#e6194B", "#aaffc3", "#ffe119"]
+
+export const shapes = [
+    {
+        name: "L Shape",
+        coordinates: [
+            [-1, 0],
+            [0, 0],
+            [0, 1],
+        ]
+    },
+    {
+        name: "Dot Shape",
+        coordinates: [
+            [0, 0],
+        ]
+    },
+    {
+        name: "Square Shape Top Left",
+        coordinates: [
+            [0, 0],
+            [-1, 0],
+            [0, -1],
+            [-1, -1],
+
+        ]
+    },
+    {
+        name: "Square Shape Top Right",
+        coordinates: [
+            [0, 0],
+            [0, 1],
+            [-1, 0],
+            [-1, -1],
+        ]
+    },
+    {
+        name: "Square Shape Bottom Left",
+        coordinates: [
+            [0, 0],
+            [0, 1],
+            [-1, 0],
+            [-1, 1],
+        ]
+    },
+    {
+        name: "Square Shape Bottom Right",
+        coordinates: [
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1],
+        ]
+    },
+    {
+        name: "T Shape",
+        coordinates: [
+            [0, 0],
+            [-1, 0],
+            [0, -1],
+            [1, -1],
+        ]
+    }
+
+]
